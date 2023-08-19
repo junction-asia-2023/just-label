9c8f908e8f1b3253d2fd579f0e505c367a1cb793
1ilsang <1ilsang@naver.com>
2023-08-20 03:02:25 +0900

    feat: Fix layout title

--- src/features/event/Container.tsx
+++ src/features/event/Container.tsx
@@ -1,10 +1,11 @@
 import { FunctionComponent } from 'react';
 
 import useEventContainer from './hooks/useEventContainer';
+import './event.scss';
 
 const EventContainer: FunctionComponent = () => {
   useEventContainer();
-  return <div></div>;
+  return <div className="event-container"></div>;
 };
 
 export default EventContainer;
