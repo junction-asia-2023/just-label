eba5bf1fb7aaa0cedfe8ffd1102728efba98353f
1ilsang <1ilsang@naver.com>
2023-08-20 01:12:00 +0900

    feat(layout): Add title history back

--- src/features/shared/layout/components/Title.tsx
+++ src/features/shared/layout/components/Title.tsx
@@ -1,5 +1,6 @@
 import { useAtom } from 'jotai';
 import { FunctionComponent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { titleAtom } from '../atom';
 
@@ -7,13 +8,20 @@ import backLogo from '/svg/back.svg';
 
 const LayoutTitle: FunctionComponent = () => {
   const [{ title, back }] = useAtom(titleAtom);
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
 
   if (!title) return <div className="layout-title" />;
 
   return (
     <div className="layout-title under-line">
       <div className="left">
-        {back && <img src={backLogo} alt="Back Logo" />}
+        {back && (
+          <img src={backLogo} alt="Back Logo" onClick={handleBackClick} />
+        )}
       </div>
       <div className="title">{title}</div>
       <div className="left" />
