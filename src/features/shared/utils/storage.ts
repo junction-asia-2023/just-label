202a94638edff19fb1cf56597b68c1ac137a382a
1ilsang <1ilsang@naver.com>
2023-08-19 13:42:03 +0900

    feat(utils): Add localstorage

---
+++ src/features/shared/utils/storage.ts
@@ -0,0 +1,25 @@
+export type STORAGE_KEY = 'user';
+
+export const getItem = (item: STORAGE_KEY) => {
+  try {
+    const value = window.localStorage.getItem(item);
+    if (!value) {
+      return false;
+    }
+    const parsedValue = JSON.parse(value);
+    return parsedValue;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
+
+export const setItem = (item: STORAGE_KEY, value: any) => {
+  try {
+    const stringValue = JSON.stringify(value);
+    window.localStorage.setItem(item, stringValue);
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
