040dd8f076590c13a8178fb95c69070ad7608928
1ilsang <1ilsang@naver.com>
2023-08-20 01:58:57 +0900

    feat(vite): Add base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import { homepage } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: homepage,
+});
