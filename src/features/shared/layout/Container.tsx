de8673ef813dedc33d08ae680a33c276e439208f
1ilsang <1ilsang@naver.com>
2023-08-19 14:56:25 +0900

    fix: rename

--- src/features/shared/layout/Container.tsx
+++ src/features/shared/layout/Container.tsx
@@ -1,9 +1,12 @@
 import { FunctionComponent, PropsWithChildren } from 'react';
+
 import './layout.scss';
+import useLayout from './hooks/useLayout';
 
 const LayoutContainer: FunctionComponent<PropsWithChildren> = ({
   children,
 }) => {
+  useLayout();
   return <div className="layout-container">{children}</div>;
 };
 
