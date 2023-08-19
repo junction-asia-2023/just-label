cc7fc5ee0d5fdb45fd7d7bf90e3d521fad852caf
1ilsang <1ilsang@naver.com>
2023-08-20 02:36:31 +0900

    feat(vite): Add production mode condition

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 import { homepage } from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: homepage,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' && homepage,
+  };
 });
