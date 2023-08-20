a5730025c39ba07743d3441538d85effe73780ef
anyl92 <yulim9592@gmail.com>
2023-08-20 10:00:29 +0900

    fix(modal): set previewState, button click event

--- src/features/shared/modal/Container.tsx
+++ src/features/shared/modal/Container.tsx
@@ -4,6 +4,8 @@ import { useResetAtom } from 'jotai/utils';
 import win from '/svg/win.svg';
 import next from '/svg/next.svg';
 import idCard from '/svg/idcard_basic.svg';
+import moodBad from '/png/mood_BAD.png';
+import moodGood from '/png/mood_GOOD.png';
 
 import { previewMoodAtom } from '../layout/atom';
 
@@ -12,10 +14,9 @@ import './modal.scss';
 
 const ModalContainer = () => {
   const [modal] = useAtom(modalAtom);
-  const [previewMood] = useAtom(previewMoodAtom);
+  const [previewMood, setPreviewMood] = useAtom(previewMoodAtom);
   const resetModal = useResetAtom(modalAtom);
 
-  console.log(previewMood);
   if (!modal.open) return null;
   const { imageType, onClose, title, description, footer, onConfirm } =
     modal as Modal;
@@ -33,25 +34,30 @@ const ModalContainer = () => {
   const handleCloseClick = () => {
     onClose?.();
     resetModal();
+    setPreviewMood({ open: false });
   };
   const handleConfirmClick = () => {
     onConfirm?.();
     resetModal();
   };
-  console.log(previewMood);
   return (
     <div className="modal-container">
       <div className="content">
         <span className="close-btn" onClick={handleCloseClick}>
           &times;
         </span>
-        {previewMood.open || modal.open ? (
+        {previewMood.open && modal.open ? (
           <>
             <div className="modal-preview-title">Preview</div>
             <div className="modal-preview-box">
               <img src={idCard} alt="preview" />
               <div className="modal-sticker-picked">
-                <img src={`/png/mood_${previewMood.value}.png`} alt="preview" />
+                {previewMood.value === 'GOOD' && (
+                  <img src={moodGood} alt="preview" />
+                )}
+                {previewMood.value === 'BAD' && (
+                  <img src={moodBad} alt="preview" />
+                )}
               </div>
             </div>
           </>
