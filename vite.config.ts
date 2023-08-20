aa49bc3e9348cf106dc9a60de2b7afd6c22393e3
1ilsang <1ilsang@naver.com>
2023-08-20 10:39:05 +0900

    feat: fix router

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { homepage } from './package.json';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
+    base: mode === 'production' ? homepage : '/',
   };
 });
